a3d595824f330c64e4bf87f1c25e3e67c2017ebb
Hector Leal <desarrollador1@soinsoluciones.com>
2020-07-12 20:51:19 -0500

    Add project files.

---
+++ BairesDevAPI/Controllers/PeopleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BairesDevAPI.Controllers
+{
+    public class PeopleController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> TopClients()
+        {
+            var json = File.ReadAllText("");
+        }
+    }
+}
---
+++ BairesDevAPI/People.cs
@@ -0,0 +1,14 @@
+﻿namespace BairesDevAPI
+{
+    public class People
+    {
+        long PersonId { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string CurrentRole { get; set; }
+        string Country { get; set; }
+        string Industry { get; set; }
+        int NumberOfRecommendations { get; set; }
+        int NumberOfConnections { get; set; }
+    }
+}
